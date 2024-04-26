26b103ff41fe54b9ba7d79359f0c41aff84fc976
edmaker89 <edmaker@gmail.com>
2024-04-26 13:51:48 -0300

    feat: test schemas and data factory

---
+++ tests/schemas/factories.py
@@ -0,0 +1,9 @@
+def product_data():
+    data = {
+        "name": "Iphone 14 pro Max",
+        "quantity": 10,
+        "price": 8.5000,
+        "status": True,
+    }
+
+    return data
---
+++ tests/schemas/test_product.py
@@ -0,0 +1,22 @@
+from uuid import UUID
+
+from pydantic import ValidationError
+import pytest
+from store.core.schemas.product import ProductIn
+from tests.schemas.factories import product_data
+
+
+def test_schemas_return_success():
+    product = ProductIn.model_validate(product_data())
+
+    assert product.name == "Iphone 14 pro Max"
+    assert isinstance(product.id, UUID)
+
+
+def test_schemas_return_raise():
+    data = product_data()
+    data.pop("status", None)
+    with pytest.raises(ValidationError) as err:
+        ProductIn.model_validate(data)
+
+    assert "missing" in str(err.value) and "status" in str(err.value)
